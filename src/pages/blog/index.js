c590a327d8cc488c8a2193dc556186638ab07034
Danny <danny@Dannys-MacBook-Pro.local>
2023-12-17 21:50:17 +0530

    Updated blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -8,7 +8,7 @@ export default function blog({ data }) {
 
   return (
     <Layout pageHeading="blog" pageTitle="blog">
-      <ul>
+      {/* <ul>
         {data.allMdx.nodes.map(node => {
           console.log(node);
           return (
@@ -22,7 +22,68 @@ export default function blog({ data }) {
           )
 
         })}
-      </ul>
+      </ul> */}
+
+      <div>
+
+        <div className="flex justify-center  px-4 font-mono">
+
+          <div className="flex w-full max-w-screen-lg">
+
+            <div className="flex flex-col flex-grow border-l border-r border-white">
+
+              <ul>
+
+                {/* <li className="flex justify-between flex-shrink-0 px-8 py-4 border-b border-white">
+                  <h1 className=" text-white text-6xl font-semibold">BLOG</h1>
+                </li> */}
+
+                {/* <li className="text-white flex-grow ">
+                  <div className="flex w-full p-8 border-b border-gray-300">
+
+                    <div className="flex flex-col flex-grow ml-4">
+
+                      <div className="grid ">
+                        <ul className="list-style-none">
+                          <li className="font-semibold">TITLE: </li>
+                          <li className="font-semibold">DATE: </li>
+                        </ul>
+                      </div>
+
+                    </div>
+
+                  </div>
+
+                </li> */}
+
+
+                <li>
+                  {data.allMdx.nodes.map(node => {
+                    console.log(node);
+                    return (
+                      <div className="flex w-full p-8 border-b border-gray-300">
+                        <div className="flex flex-col flex-grow ml-4">
+                          <div className="grid ">
+                            <article key={node.id}>
+                              <Link to={`/blog/${node.id}`}>
+                                <h2 className='text-4xl font-extrabold text-white p-1 hover:text-blue-500 hover:underline '>{node.frontmatter.title} </h2>
+                              </Link>
+                              <p className='text-[#FADEBD]'>{node.frontmatter.date}</p>
+                            </article>
+                          </div></div></div>
+                    )
+
+                  })}
+                </li>
+
+
+
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
     </Layout>
 
   )
