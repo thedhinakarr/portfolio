6d1d39b16cdcc07d3873e330da2efcc11f2f2e55
Danny <danny@Dannys-MacBook-Pro.local>
2023-12-17 19:34:34 +0530

    Updated home

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,11 @@ export default function Layout({pageTitle, pageHeading,children}) {
             <title>
                 {pageTitle} 
             </title>
-            <p></p>
             <nav>
-                <div className="container mx-auto flex justify-between items-center">
+                <div className="container mx-auto flex justify-between">
                     <div className='hover:underline'><Link to="/">Home</Link></div>
-                    <div className='hover:underline'><Link to="/about">About</Link></div>
                     <div className='hover:underline'><Link to="/blog">Blog</Link></div>
+                    <div className='hover:underline'><Link to="/about">About</Link></div>
                 </div>
             </nav>
             <h1 className='text-green-300 text-6xl'>{pageHeading}</h1>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,32 @@
 import * as React from "react"
 import Layout from '../components/layout'
 import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from "gatsby"
 
 export default function index() {
   return (
-   <Layout pageHeading="Home" pageTitle="Home">
-    ;klaj;dfjka;jdf;akjd;fljasdkjfak;j
-    <StaticImage src='https://www.gatsbyjs.com/static/63c1a5b6a0b9afa75315883fea087559/b5a09/plugin-process.png' alt='failed'/>
-   </Layout>
+    //  <Layout pageHeading="Home" pageTitle="Home">
+    //   ;klaj;dfjka;jdf;akjd;fljasdkjfak;j
+    //  </Layout>
+    <div className="border-[#FADEBD] min-h-screen flex items-center justify-center font-semibold">
+      <div className="max-w-md">
+        <p className="text-lg text-justify pb-4">
+          {/* YO, I'm<text className="text-blue-500"> Dhinakarr,</text> */}
+          Hey! I'm<text className="text-blue-500 text-2xl"> Dhinakarr, </text>
+          I like computers. In the realm of algorithms and coding, I navigate through lines of logic to build robust solutions.<br/><br/>
+          Check out my blog which documents my journey of continuous learning and exploration as I delve into the intricacies of the digital world.<br /><br />
+        </p>
+        <nav>
+          <div className="container mx-auto flex justify-between font-semibold text-2xl ">
+            <div className='hover:underline hover:text-[#FADEBD]'><Link to="/blog">Blog</Link></div>
+            <div className='hover:underline  hover:text-[#FADEBD]'><Link to="/about">Resumé</Link></div>
+            <div className='hover:underline  hover:text-[#FADEBD]'><Link to="https://github.com/thedhinakarr" onClick="_blank">Github</Link></div>
+          </div>
+        </nav>
+      </div>
+
+    </div>
+
   )
 }
 
