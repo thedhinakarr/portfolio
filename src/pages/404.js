c478f36cc0269ebe78383f9a2ee28da3817c9521
Danny <danny@Dannys-MacBook-Pro.local>
2024-01-07 20:17:14 +0530

    CSS

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,20 +1,6 @@
 import * as React from "react"
 import { Link } from "gatsby"
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
 const codeStyles = {
   color: "#8A6534",
   padding: 4,
