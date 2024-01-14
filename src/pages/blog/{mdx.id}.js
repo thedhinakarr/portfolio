3ae5a73438952d57a2e656a5233fde5459bac51b
Danny <danny@Dannys-MacBook-Pro.local>
2024-01-14 19:43:28 +0530

    vii,9

--- src/pages/blog/{mdx.id}.js
+++ src/pages/blog/{mdx.id}.js
@@ -45,8 +45,8 @@ query MyQuery($id: String) {
   mdx(id: {eq: $id}) {
     id
     frontmatter {
-      date
       title
+      date
       hero_image_alt
       hero_image_credit_link
       hero_image_credit_text
